da209d92b20e5ec4a522d3e5d04c443e168512a2
ridouku <ridouku@gmail.com>
2021-09-15 12:32:19 -0500

    feat: KPI-21 Change pipelines

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import site_under_construction from "./site_under_construction.png"
+import './App.css';
+import {Grid} from "@material-ui/core";
+
+function App() {
+    return (
+        <div className="App">
+            <Grid container style={{textAlign: "center"}} fixed={true}>
+                <Grid item xs={12}>
+                    <h1 style={{color: "black", fontWeight: 700}}>
+                        KPINETWORKS IS UNDER CONSTRUCTION
+                    </h1>
+                </Grid>
+                <Grid item xs={12} justify="space-between"
+                      style={{
+                          backgroundImage: `url(${site_under_construction})`,
+                          backgroundSize: 'contain',
+                          backgroundRepeat: 'no-repeat',
+                          height: 400,
+                          backgroundPosition: "center"
+                      }}>
+                </Grid>
+                <Grid item xs={12}>
+                    <p style={{color: "black"}}>
+                        Our website going to be ready soon.
+                    </p>
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
+
+export default App;
