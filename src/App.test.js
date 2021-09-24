d3920ebf1dc36e05467701772be1a55903cf5ab9
ridouku <ridouku@gmail.com>
2021-09-24 18:07:30 -0500

    feat: KPI-31 unit test fixed

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/KPINETWORKS IS UNDER CONSTRUCTION/i);
+  const linkElement = screen.getByText(/KPINETWORK IS UNDER CONSTRUCTION/i);
   expect(linkElement).toBeInTheDocument();
 });
