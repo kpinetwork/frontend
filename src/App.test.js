fb29f0cc23b25e6631c98fe8c47a1c210ee7454d
ridouku <ridouku@gmail.com>
2021-09-29 09:39:33 -0500

    feat: KPI-31 Unit test improved

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/KPINETWORK IS UNDER CONSTRUCTION/i);
-  expect(linkElement).toBeInTheDocument();
+    render(<App/>);
+    const linkElement = screen.getByTestId('tittle')
+    expect(linkElement).toBeTruthy();
 });
