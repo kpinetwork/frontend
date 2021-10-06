e297cc983259a6bfcfea06174fe9181a4b75640d
kellycastrof <castrofigueroakelly@gmail.com>
2021-10-06 15:18:23 -0500

    test: KPI-35 test sonar cloud

--- apps/cloud/src/app/modules/custom/hello-world/hello-world.js
+++ apps/cloud/src/app/modules/custom/hello-world/hello-world.js
@@ -3,7 +3,7 @@ import {Grid} from "@material-ui/core";
 function HelloWorld() {
     return (
         <div className="HelloWorld">
-            <Grid container style={{textAlign: "center"}} fixed={true}>
+            <Grid container style={{textAlign: "center"}}>
                 <Grid item xs={12}>
                     <h1 data-testid="tittle" style={{color: "black", fontWeight: 700}}>
                         Hello World
