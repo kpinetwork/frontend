bf3bfdf13852f038e290181cc75dd6020ca09d31
kellycastrof <castrofigueroakelly@gmail.com>
2021-10-06 14:57:56 -0500

    test: KPI-35 change base dir with custom folder

---
+++ apps/cloud/src/app/modules/custom/hello-world/hello-world.test.js
@@ -0,0 +1,8 @@
+import {render, screen} from '@testing-library/react';
+import HelloWorld from './hello-world';
+
+test('renders learn react link', () => {
+    render(<HelloWorld/>);
+    const linkElement = screen.getByTestId('tittle')
+    expect(linkElement).toBeTruthy();
+});
