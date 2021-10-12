a6cdc07a296b02a0499ad757ce4269f4861fa9ce
ridouku <ridouku@gmail.com>
2021-10-12 12:30:17 -0500

    feat: changed branch to check KPI-36

--- apps/cloud/src/app/modules/custom/components/auth/aws-exports.js
+++ apps/cloud/src/app/modules/custom/components/auth/aws-exports.js
@@ -17,5 +17,4 @@ const awsmobile = {
   federationTarget: 'COGNITO_USER_POOLS',
 };
 
-
 export default awsmobile;
--- libs/react/components/src/lib/ksFilledCircle/ksFilledCircle.tsx
+++ libs/react/components/src/lib/ksFilledCircle/ksFilledCircle.tsx
@@ -7,11 +7,14 @@ import { FilledCircleProps } from './ksFilledCircle.model';
 const KsFilledCircle = ({ color, percentage }: FilledCircleProps): ReactElement => {
   return (
     <div>
-      <div className='ks-filled-outer-circle' style={{border: `${color} var(--pm-7XS) solid`}}>
-        <div className='ks-filled-inner-circle' style={{backgroundColor: color, height: `${percentage}%`, width: `${percentage}%`}}></div>
+      <div className="ks-filled-outer-circle" style={{ border: `${color} var(--pm-7XS) solid` }}>
+        <div
+          className="ks-filled-inner-circle"
+          style={{ backgroundColor: color, height: `${percentage}%`, width: `${percentage}%` }}
+        ></div>
       </div>
     </div>
   );
-}
+};
 
 export default React.memo(KsFilledCircle);
