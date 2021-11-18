049e1953bd4e693858660669d2be6d9b667de8c0
ridouku <ridouku@gmail.com>
2021-11-18 12:19:18 -0500

    feat: KPI-59 script changed

--- .github/scripts/remove_untagged_images_for_ecr.sh
+++ .github/scripts/remove_untagged_images_for_ecr.sh
@@ -7,30 +7,16 @@ type aws > /dev/null 2>&1 && type jq > /dev/null 2>&1 || {
   exit 1
 }
 
-repositories=$(aws ecr describe-repositories --query 'repositories[*].repositoryName' | tr -d "[\",\t\n\r\"]")
+repository="$1_kpinetwork_kleeen_api_repo"
+echo "$repository: Deleting untagged images"
 
-for repository in $repositories; do
+response=$(aws ecr list-images --repository-name $repository --filter tagStatus=UNTAGGED --max-items 100)
+untaggedImages=$(echo $response | jq -c '.imageIds')
+
+if [ "$untaggedImages" == "[]" ];
+then
+  echo "$repository: Untagged image not found"
+else
   echo "$repository: Deleting untagged images"
-
-  nextToken="initialized"
-
-  while [ "$nextToken" != "null" ]; do
-    if [ "$nextToken" == "initialized" ]; then
-      response=$(aws ecr list-images --repository-name $repository --filter tagStatus=UNTAGGED --max-items 100)
-    else
-      response=$(aws ecr list-images --repository-name $repository --filter tagStatus=UNTAGGED --max-items 100 --starting-token $nextToken)
-    fi
-
-    nextToken=$(echo $response | jq -r '.NextToken')
-    untaggedImages=$(echo $response | jq -c '.imageIds')
-
-    if [ "$untaggedImages" == "[]" ]; then
-      echo "$repository: Untagged image not found"
-      break
-    fi
-
-    aws ecr batch-delete-image --repository-name $repository --image-ids $untaggedImages
-  done
-
-  echo "$repository: Deleted untagged images"
-done
+  aws ecr batch-delete-image --repository-name $repository --image-ids $untaggedImages
+fi
